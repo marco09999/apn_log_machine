b78031fe51a3086a73e70754ded8d848a262300b
marco09999 <archambault_ma@hotmail.fr>
2022-06-15 08:56:38 -0400

    First release on Git

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ for machines in apn_mach:
         with open(machines["path"], "r") as tf:
             line_list = tf.read().split("\n")
             tf.close()
-
         # Step 1: Transform START and END BLOC into lines
         step_1 = []
         head_1 = ["Status", "Date", "Time", "Pallet", "Name", "Post Version", "Post Date", "Force Version", "Force Date", "Otr",
@@ -15,7 +14,6 @@ for machines in apn_mach:
         for i in range(0, len(line_list)):
             if len(Header.line_decode_step_1(line_list, i)) > 1:
                 step_1.append(Header.line_decode_step_1(line_list, i))
-
         # Step 2: Transform START and END lines into execution
         step_2 = []
         head_2 = ["Name", "Cycle time (min)", "Post", "Force", "Otr", "Start Date", "Start Time", "End Date", "End time"]
@@ -23,7 +21,6 @@ for machines in apn_mach:
         for i in range(0, len(step_1)-1):
             if len(Header.line_decode_step_2(step_1, i)) > 1:
                 step_2.append(Header.line_decode_step_2(step_1, i))
-
         # Step 2.5: Add identifier at the end of each execution
         step_2_5 = step_2
         head_2_5 = ["Name", "Cycle time (min)", "Post", "Force", "Otr", "Start Date", "Start Time", "End Date",
@@ -31,7 +28,6 @@ for machines in apn_mach:
         step_2_5[0] = head_2_5
         for i in range(1, len(step_2_5)):
             step_2[i].append(step_2[i][0] + "-" + step_2[i][2] + "-" + step_2[i][3] + "-" + step_2[i][4])
-
         # Step 3: Compress into single line and number of execution for each unique program
         head_3 = ["Program Name", "Cycle time (min)", "Execution", "Start Date", "Start time", "End date", "End time",
                   "Post", "Force", "Otr", "Machine"]
@@ -39,47 +35,7 @@ for machines in apn_mach:
         # Add machine Number
         for i in step_3:
             i.append(machines["name"])
-
-
         for i in step_3:
             print(i)
 
         print("\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
