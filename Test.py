67e66a04552c217df8861e084d0cbaa916d9b6ad
marco09999 <archambault_ma@hotmail.fr>
2022-06-29 19:31:24 -0400

    test github

--- Test.py
+++ Test.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 root = Tk()
-root.title("calculator")
+root.title("test")
 
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
